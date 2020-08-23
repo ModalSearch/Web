dd4cf964b779b142d1cc4374a76198d2c4875b44
Max S <sunm@hey.com>
2020-08-22 22:50:50 -0700

    UI tweaks for dataset headers

--- src/components/ResultsView.js
+++ src/components/ResultsView.js
@@ -8,10 +8,24 @@ const ResultsView = ({ data, theme }) => {
         return <div>Connecting...</div>
     }
 
+    const annotation_elem = data.annotation.map(anno => {
+        return (
+            <a style={{
+                // backgroundColor: theme.base02,
+                color: theme.base06,
+                padding: 5,
+            }} href={`?q=${anno.link}`}>
+                {anno.text}
+            </a>
+        );
+    });
+
     if (data.type === 'terminal') {
         return (
-            <div style={{ backgroundColor: theme.base01, flex: 1, marginTop: 10 }}>
-                <div style={{ backgroundColor: theme.base02, padding: 5 }}>{data.annotation}</div>
+            <div style={{ backgroundColor: theme.base01, flex: 1, marginTop: 10, }}>
+                <div style={{fontSize: 18, backgroundColor: theme.base01, padding: 5}}>
+                    {annotation_elem}
+                </div>
                 <div style={{ padding: 5 }}>
                     {data.data.map(item => <Result source={item.id} timestamp={item.timestamp} key={item.id} data={item.data} theme={theme}></Result>)}
                 </div>
@@ -25,17 +39,19 @@ const ResultsView = ({ data, theme }) => {
             <div style={{
                 flex: 1,
                 backgroundColor: theme.base01,
-                paddingLeft: 10,
-                paddingTop: 10
+                // paddingLeft: 10,
+                // paddingTop: 10
             }}>
-                <span style={{
-                    width: '100%',
+                <div style={{
+                    // width: '100%',
                     flex: 1,
+                    padding: 3,
                     backgroundColor: theme.base02
                 }}>
-                    {data.annotation}
-                </span>
-                <div>
+                    {/* {JSON.stringify(data.annotation)} */}
+                    {annotation_elem}
+                </div>
+                <div style={{paddingLeft: 10, paddingTop: 10}}>
                     {data.members.map(item => {
                         return (
                             <div>
